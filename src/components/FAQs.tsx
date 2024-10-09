b7a159d0f831d90e340d2b95b57fe3d3ca9544b4
Austin Nicola Ardisaputra <ardisaputra.2003@gmail.com>
2024-10-09 16:21:40 -0700

    Language fix for Hero section and FAQ

--- src/components/FAQs.tsx
+++ src/components/FAQs.tsx
@@ -1,67 +1,79 @@
-"use client"
+"use client";
 import { useState } from "react";
 import PlusIcon from "../assets/icons/plus.svg";
 import MinusIcon from "../assets/icons/minus.svg";
 import clsx from "clsx";
-import {motion , AnimatePresence} from 'framer-motion';
+import { motion, AnimatePresence } from "framer-motion";
 const items = [
   {
     question: "Why should you use VisaLink?",
     answer:
-      "A large part of the US work visa application/management process is tediously manual, costly, and unnecessarily time consuming. Companies using VisaLink can begin the visa petitioning process right away without having to wait for a case manager or any onboarding. As a SaaS product, we are also able to have a more transparent and affordable pricing model that isn't \"case by case\" as most competitors do. Companies can now also track in real time the visa status of their current employees and new candidates which is a pain point that we identified particularly for large organizations.",
+      'The U.S. work visa application and management process is often highly manual, costly, and time-consuming. With VisaLink, companies can initiate the visa petition process immediately, without waiting for a case manager or undergoing lengthy onboarding. As a SaaS solution, we offer a more transparent and cost-effective pricing model, avoiding the traditional "case-by-case" approach used by most competitors. Additionally, VisaLink enables real-time tracking of visa statuses for both current employees and new candidates—addressing a significant challenge, particularly for large organizations.',
   },
   {
     question: "Why does VisaLink work?",
     answer:
-      "Many work visa types such as the TN visa, E-3 visa, require a very standardized set of documents such that the process for applying to these visas can be easily automated. Based on our research, failures or rejections to these visas happen for easily fixable reasons such as typos and field mismatches, things our AI can detect.",
+      "Many work visa types, such as the TN and E-3 visas, require a standardized set of documents, making the application process well-suited for automation. Our research shows that denials or rejections often occur due to simple, easily correctable errors like typos and mismatched fields—issues that our AI is designed to detect and prevent.",
   },
   {
     question: "Why not stick to the traditional way of work visa sponsorships?",
     answer:
-      "Legal fees can start at $5,000 and quickly reach $15,000 or more for each visa (source: Rippling). Not to mention, the complete delegation of work to 3rd parties leaves you effectively blind towards the state of the process at any time. Why waste weeks of waiting for an update by a lawyer when you can actually have a new hire start working instantly?",
+      "Legal fees for visa processing can range from $5,000 to $15,000 or more per visa (source: Rippling). Additionally, outsourcing the entire process to third parties often leaves you without visibility into the status at any given time. Instead of waiting weeks for updates from a lawyer, our platform will drive you to the same destination in a fraction of the time and cost.",
   },
 ];
 
-const AccordinationItem = ({question, answer}:{question:string, answer: string}) => {
-  const[isOpen, setIsOpen] = useState(false);
-  return(
-   
-    <div className=" py-7 border-b border-white/30" onClick={() => setIsOpen(!isOpen)}>
-    <div className="flex items-center ">
-      <span className="flex-1 text-lg font-bold">{question}</span>
-      {isOpen ? <MinusIcon /> :<PlusIcon />}
-      
+const AccordinationItem = ({
+  question,
+  answer,
+}: {
+  question: string;
+  answer: string;
+}) => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div
+      className=" py-7 border-b border-white/30"
+      onClick={() => setIsOpen(!isOpen)}
+    >
+      <div className="flex items-center ">
+        <span className="flex-1 text-lg font-bold">{question}</span>
+        {isOpen ? <MinusIcon /> : <PlusIcon />}
       </div>
       <AnimatePresence>
-      {isOpen && (
-        <motion.div 
-        initial={{opacity: 0, height: 0, marginTop: 0}}
-        animate={{opacity: 1, height: "auto" , marginTop:'16px'}}
-        exit={{opacity: 0, height: 0, marginTop: 0}}
-          >{answer}</motion.div>
-
-      )}
+        {isOpen && (
+          <motion.div
+            initial={{ opacity: 0, height: 0, marginTop: 0 }}
+            animate={{ opacity: 1, height: "auto", marginTop: "16px" }}
+            exit={{ opacity: 0, height: 0, marginTop: 0 }}
+          >
+            {answer}
+          </motion.div>
+        )}
       </AnimatePresence>
-    
-  </div>
-  
-    
-  )
-}
+    </div>
+  );
+};
 
 export const FAQs = () => {
   return (
-    <div id="faq" className="bg-black text-white py-[72px] sm:py-24 bg-gradient-to-b from-[#003366] to-black ">
+    <div
+      id="faq"
+      className="bg-black text-white py-[72px] sm:py-24 bg-gradient-to-b from-[#003366] to-black "
+    >
       <div className="container">
         <h2 className="text-5xl sm:text-6xl sm:w-[648px] mx-auto text-center text-white tracking-tighter">
           Frequently Asked Questions
         </h2>
         <div className="mt-12 max-w-[648px] mx-auto">
-         {items.map(({question, answer}) => (
-            <AccordinationItem question={question} answer={answer} key={question}/>
-         ))}
+          {items.map(({ question, answer }) => (
+            <AccordinationItem
+              question={question}
+              answer={answer}
+              key={question}
+            />
+          ))}
         </div>
       </div>
     </div>
-  )
+  );
 };
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,35 +1,45 @@
-"use client"
-import CursorImage from '../assets/images/cursor.png'
-import ArrowIcon from '../assets/icons/arrow-w.svg'
-import MessageImage from '../assets/images/message.png'
-import Image from 'next/image';
-import {motion} from 'framer-motion'
-import { AnimatedGradientTextDemo } from './animatedtext';
+"use client";
+import CursorImage from "../assets/images/cursor.png";
+import ArrowIcon from "../assets/icons/arrow-w.svg";
+import MessageImage from "../assets/images/message.png";
+import Image from "next/image";
+import { motion } from "framer-motion";
+import { AnimatedGradientTextDemo } from "./animatedtext";
 
 export const Hero = () => {
   return (
-    <div id="demo" className="bg-black text-white bg-[linear-gradient(to_bottom,#000,#003366_34%,#0066CC_65%,#66CCFF_82%)] py-[72px] sm:py-24 relative overflow-clip">
+    <div
+      id="demo"
+      className="bg-black text-white bg-[linear-gradient(to_bottom,#000,#003366_34%,#0066CC_65%,#66CCFF_82%)] py-[72px] sm:py-24 relative overflow-clip"
+    >
       <div className="absolute h-[375px] w-[750px] sm:w-[1536px] sm:h-[768px] lg:w-[2400px] llg:h-[800px] rounded-[100%] bg-black left-1/2 -translate-x-1/2 border border-[#0066CC] bg-[radial-gradient(closest-side,#000_82%,#0066CC)] top-[calc(100%-96px)] sm:top-[calc(100%-120px)]"></div>
-    <div className="container relative">
-      <div className="flex items-center justify-center -mt-10">
-        
+      <div className="container relative">
+        <div className="flex items-center justify-center -mt-10"></div>
+        <div className="flex justify-center mt-8 ">
+          <div className="inline-flex relative">
+            <h1 className="text-7xl sm:text-9xl font-bold tracking-tightner text-center inline-flex mb-2">
+              <br />
+              VisaLink <br />
+            </h1>
+          </div>
+        </div>
+        <div className="flex justify-center">
+          <p className="text-xl text-center mt-8 max-w-[29rem]">
+            Don&apos;t let time and costs be a barrier to hiring talent!
+            Effortlessly start and complete work visa applications for new
+            hires, track your employees&apos; visa statuses in real-time, and
+            receive timely alerts for upcoming visa expirations—all through our
+            AI-powered platform.
+          </p>
+        </div>
+        <div className="flex justify-center mt-8">
+          <a href="https://forms.gle/PGPomN7oAHzYTJ3j9">
+            <button className="bg-white text-black py-3 px-5 rounded-lg font-medium">
+              Sign up now
+            </button>
+          </a>
+        </div>
       </div>
-      <div className="flex justify-center mt-8 ">
-      <div className="inline-flex relative">
-      <h1 className='text-7xl sm:text-9xl font-bold tracking-tightner text-center inline-flex mb-2'><br/>VisaLink <br /></h1>
-      </div>
-      </div>
-      <div className="flex justify-center">
-      <p className='text-xl text-center mt-8 max-w-md'>Don&apos;t let time and costs be a barrier to hiring talent! Start and complete work visa applications for your new hires, stay up to date with your employees&apos; visa statuses, get notified for upcoming visa expiries, all in one AI-powered platform.</p>
-      </div>
-      <div className="flex justify-center mt-8">
-      <a href="https://forms.gle/PGPomN7oAHzYTJ3j9"><button className='bg-white text-black py-3 px-5 rounded-lg font-medium'>Sign up now</button></a>
-      </div>
-
-
     </div>
-    
-
-    </div>
-  )
+  );
 };
